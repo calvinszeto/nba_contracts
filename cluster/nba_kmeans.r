aac89866161af100ee03e27dbf0b4138de3d4e7e
Calvin Szeto <szeto.calvin@gmail.com>
2014-04-14 19:37:29 -0500

    Add distance metric.

--- cluster/nba_kmeans.r
+++ cluster/nba_kmeans.r
@@ -5,16 +5,51 @@
 # Named "nba_kmeans" in order to distinguish from the 
 # kmeans function that comes with R.
 
-nba_kmeans <- function(dat) {
+max_clusters <- 50
+max_iterations <- 30
+
+nba_kmeans <- function(dat, id="Rk") {
     # Lowest RMSE so far
     best_rmse <- -1
     # Number of clusters that resulted in best score so far
     best_num <- -1
-    # Vector that holds on to best clustering so far
+    # Data frame that holds on to best clustering so far
     best_clustering <- NULL 
 
-    for (i in 1:50) {
-        
-    }
+    # Remove non-numeric columns to get a matrix
+    mat <- dat[ , sapply(dat,is.numeric) ]
+    
+    for (k in 1:max_clusters) {
+        # Keep in mind that the first column is the ID column
+        num_variables = length(mat) - 1
+        # Initialize cluster centers
 
+        for (i in 1:max_iterations) {
+            # Set cluster variable for each row as the closest center  
+            # Recalculate cluster centers
+        }
+    }
+    
+    # Merge the clusters with the original dataset
+    return(merge(dat,best_clustering, by="Rk"))
+}
+
+# row is the vector to be clustered
+# centers is the matrix of cluster centers
+euclidean_closest <- function(row, centers) {
+    # TODO: Consider ID variable
+    min_distance <- -1
+    best_cluster <- 0
+    for (c in 1:length(centers[,1])) {
+        distance <- 0 
+        for (v in 1:num_variables) {
+            distance = distance + (row[v] - centers[c,v]) ** num_variables
+        }
+        distance = distance ** (1/num_variables)
+        if (min_distance < 0 | distance < min_distance) {
+            min_distance <- distance
+            best_cluster <- c
+        }
+    }
+    return(c)
 }
--- preprocess/preprocess.r
+++ preprocess/preprocess.r
@@ -1,22 +1,18 @@
 seasons_included <- c('03-04','04-05','05-06','06-07','07-08','08-09','09-10','10-11','11-12','12-13')
+types_included <- c('per_game', 'per_minute', 'totals')
 
-totals <- function(season) {
-    if(!(season %in% seasons_included)) {
+# Returns a dataset from Basketball Reference (http://www.basketball-reference.com)
+# for a particular season
+bref_season <- function(season, type='per_game') {
+    if(!(season %in% seasons_included) | !(type %in% types_included)) {
         return(NULL)
     }
-    file <- paste(c('./data/',season,'/totals.csv'), collapse='')
-    totals <- read.csv(file)
-    return(remove_duplicates(totals))
+    file <- paste(c('./data/',season,'/',type,'.csv'), collapse='')
+    data <- read.csv(file)
+    return(remove_duplicates(data))
 }
 
-per_game <- function(season) {
-    return(NULL)
-}
-
-per_minute <- function(season) {
-    return(NULL)
-}
-
+# Removes split rows for players who played for multiple teams in the season
 remove_duplicates <- function(data) {
     movers <- data[data$Tm == 'TOT', 'Player'] 
     return(data[!(data$Player %in% movers) | (data$Tm == 'TOT'),])
